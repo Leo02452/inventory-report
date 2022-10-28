a00b30cc6854335f186d3951accd780cd2110be1
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-28 16:13:17 -0300

    test: implements report method in product class

--- tests/product_report/test_product_report.py
+++ tests/product_report/test_product_report.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_relatorio_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        'any-name',
+        'any-company',
+        '11/12/2001',
+        '11/12/2021',
+        'any-serie-number',
+        'any-instructions'
+    )
+
+    assert new_product.__repr__() == "O produto any-name" \
+        " fabricado em 11/12/2001" \
+        " por any-company com validade" \
+        " até 11/12/2021" \
+        " precisa ser armazenado any-instructions."
