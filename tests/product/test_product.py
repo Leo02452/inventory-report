5b4113a34f22ba1ceacf572d2d6a4fff290a7c61
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-28 16:12:34 -0300

    test: replace implementation to verifiy attributes

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -6,14 +6,16 @@ def test_cria_produto():
         1,
         'any-name',
         'any-company',
-        'any-fabric-date',
-        'any-validation-date',
+        '11/12/2001',
+        '11/12/2021',
         'any-serie-number',
         'any-instructions'
     )
 
-    assert new_product.__repr__() == "O produto any-name" \
-        " fabricado em any-fabric-date" \
-        " por any-company com validade" \
-        " até any-validation-date" \
-        " precisa ser armazenado any-instructions."
+    assert new_product.id == 1
+    assert new_product.nome_do_produto == 'any-name'
+    assert new_product.nome_da_empresa == 'any-company'
+    assert new_product.data_de_fabricacao == '11/12/2001'
+    assert new_product.data_de_validade == '11/12/2021'
+    assert new_product.numero_de_serie == 'any-serie-number'
+    assert new_product.instrucoes_de_armazenamento == 'any-instructions'
