2930a3b1c86cac9065b4c0e59a9eea48a7fdc2af
Leo Araujo <leonardo_02452@hotmail.com>
2022-10-28 15:52:46 -0300

    test: implements Product class

--- tests/product/test_product.py
+++ tests/product/test_product.py
@@ -1,5 +1,19 @@
-# from inventory_report.inventory.product import Product
+from inventory_report.inventory.product import Product
 
 
 def test_cria_produto():
-    pass  # Seu teste deve ser escrito aqui
+    new_product = Product(
+        1,
+        'any-name',
+        'any-company',
+        'any-fabric-date',
+        'any-validation-date',
+        'any-serie-number',
+        'any-instructions'
+    )
+
+    assert new_product.__repr__() == "O produto any-name" \
+        " fabricado em any-fabric-date" \
+        " por any-company com validade" \
+        " até any-validation-date" \
+        " precisa ser armazenado any-instructions."
