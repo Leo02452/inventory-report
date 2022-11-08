5ec7beb9c91c122b5e9236ae1fc3c37be4d2eea3
Leo Araujo <leonardo_02452@hotmail.com>
2022-11-08 16:31:36 -0300

    feat: add get report method on Inventory class

--- inventory_report/inventory/inventory_refactor.py
+++ inventory_report/inventory/inventory_refactor.py
@@ -1,5 +1,7 @@
 from collections.abc import Iterable
 from inventory_report.inventory.inventory_iterator import InventoryIterator
+from inventory_report.reports.simple_report import SimpleReport
+from inventory_report.reports.complete_report import CompleteReport
 
 
 class InventoryRefactor(Iterable):
@@ -10,5 +12,12 @@ class InventoryRefactor(Iterable):
     def __iter__(self):
         return InventoryIterator(self.data)
 
-    def import_data(self, path, type):
+    def get_report(self, report_type):
+        if report_type == 'simples':
+            return SimpleReport.generate(self.data)
+        elif report_type == 'completo':
+            return CompleteReport.generate(self.data)
+
+    def import_data(self, path, report_type):
         self.data.extend(self.importer.import_data(path))
+        return self.get_report(report_type)
